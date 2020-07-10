4a45d35906a8996bcbca44d105c58b2f51d8f4de
Roman Cebula <ceboolion@gmail.com>
2020-07-10 20:00:31 +0200

    addition of imageView, configuration of imageView

--- Day90ChallengeUIImage/ViewController.swift
+++ Day90ChallengeUIImage/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let imageView: UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        configureImageView()
     }
 
     private func configureView(){
         view.backgroundColor = .white
         title = "Meme App"
     }
-
+    
+    private func configureImageView(){
+        view.addSubview(imageView)
+        imageView.backgroundColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
 }
 

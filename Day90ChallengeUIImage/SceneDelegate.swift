14dbf653b4c66db51e4a26eb5c5645e050a9fa7a
Roman Cebula <ceboolion@gmail.com>
2020-07-10 19:52:14 +0200

    initial commit

---
+++ Day90ChallengeUIImage/SceneDelegate.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let rootVC = ViewController()
+        let nav = UINavigationController(rootViewController: rootVC)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Day90ChallengeUIImage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Day90ChallengeUIImage
+//
+//  Created by Ceboolion on 10/07/2020.
+//  Copyright © 2020 Ceboolion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
+
+    private func configureView(){
+        view.backgroundColor = .white
+        title = "Meme App"
+    }
+
+}
+
